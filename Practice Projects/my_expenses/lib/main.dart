40efe8d5b7096c93cd9640146539bcfea462e9b8
talhaSaqib <tsaqib8373@gmail.com>
2020-10-04 18:33:24 +0500

    folder 4, video 24

--- Practice Projects/my_expenses/lib/main.dart
+++ Practice Projects/my_expenses/lib/main.dart
@@ -1,64 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:myexpenses/transaction.dart';
+import './widgets/user_transaction.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'My Expenses', home: MyHomePage());
+  }
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('My Expenses'),
       ),
-      body: Column(
-        children: <Widget>[
-          Card(
-            child: Text('Chart'),
-          ),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                child: Row(children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 2),
-                    ),
-                    child: Text(
-                      tx.amount.toString(),
-                    ),
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Text(tx.title),
-                      Text(
-                        tx.date.toString(),
-                      )
-                    ],
-                  )
-                ]),
-              );
-            }).toList(),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Card(
+              child: Text('Chart'),
+            ),
+            UserTransactions()
+          ],
+        ),
       ),
     );
   }
---
+++ Practice Projects/my_expenses/lib/widgets/transaction_list.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((tx) {
+            return Card(
+              child: Row(children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple, width: 2),
+                  ),
+                  child: Text(
+                    '\$${tx.amount}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tx.title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(tx.date),
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                )
+              ]),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
